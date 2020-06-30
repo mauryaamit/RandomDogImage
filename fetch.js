050489fa53ed18b8dfcabe7485fcc2d8d1224a37
Amit Maurya <amitmauryammm1999@gmail.com>
2020-06-30 21:56:27 +0530

    first commit

---
+++ fetch.js
@@ -0,0 +1,31 @@
+$.getJSON('https://dog.ceo/api/breed/hound/images',function(data){
+            var index1 = Math.floor(Math.random() * 1000);
+            var index2 = Math.floor(Math.random() * 1000);
+            var index3 = Math.floor(Math.random() * 1000); 
+            $('.get_img1').attr('src', data.message[index1]);
+            $('.get_img2').attr('src', data.message[index2]);
+            $('.get_img3').attr('src', data.message[index3]);
+
+            $( "#button" ).click(function() {
+                var index1 = Math.floor(Math.random() * 1000);
+                var index2 = Math.floor(Math.random() * 1000);
+                var index3 = Math.floor(Math.random() * 1000); 
+                $('.get_img1').attr('src', data.message[index1]);
+                $('.get_img2').attr('src', data.message[index2]);
+                $('.get_img3').attr('src', data.message[index3]);
+            });
+        });
+
+        $.getJSON('http://api.icndb.com/jokes/latest',function(data){            
+            console.log(data);
+            var index = Math.floor(Math.random() * 574);
+            var get_joke = data.value[index].joke;
+            $('.joke').html("Joke : " + get_joke);
+
+            $( "#button" ).click(function(){
+                index = Math.floor(Math.random() * 574);
+                get_joke = data.value[index].joke;
+                $('.joke').html("Joke : " + get_joke);
+            });
+               
+        });
